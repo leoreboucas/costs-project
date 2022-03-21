a791f6a47986a2367983392438186427f9e71f54
Leonardo Rebouças <leoabcblm@gmail.com>
2022-03-21 16:02:49 -0300

    backend

--- siteapp/src/components/project/ProjectForm.js
+++ siteapp/src/components/project/ProjectForm.js
@@ -1,16 +1,45 @@
-function ProjectForm(){
+import styles from './ProjectForm.module.css'
+
+import { useState, useEffect } from 'react'
+
+import Input from '../form/Input';
+import Select from '../form/Select';
+import SubmitButton from '../form/SubmitButton';
+
+function ProjectForm({ btnText }){
+    const [categories, setCategories] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/categories', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+            .then((resp) => resp.json())
+            .then((data) => {
+                setCategories(data)
+            })
+            .catch((err) => console.log(err))
+    }, [])
+
+
     return(
-        <form>
-            <div><input type="text" placeholder='Insira o nome do projeto' /></div>
-            <div><input type="number" placeholder='Insira o orçamento total'/></div>
-            <div>
-                <select name="category_id">
-                    <option disabled selected>Seleciona a categoria</option>
-                </select>
-            </div>
-            <div>
-                <input type="submit" value="Criar Projeto" />
-            </div>
+        <form className={styles.form} >
+            <Input 
+            type='text' placeholder='Insira o nome do projeto' name='name' 
+            text='Nome do Projeto'/>
+            
+            <Input 
+            type='number' placeholder='Insira o orçamento total' 
+            text='Orçamento do Projeto'
+            name='budget'
+            />
+            
+            <Select name='category_id' text='Selecione a categoria' option={categories} />
+            
+            <SubmitButton 
+            text={btnText} />
         </form>
     );
 }
