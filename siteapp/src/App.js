b5b5add9bc88f9abaffd68267c367f76cdfd043d
Leonardo Rebouças <leoabcblm@gmail.com>
2022-03-20 21:46:52 -0300

    Componentes e Layout

--- siteapp/src/App.js
+++ siteapp/src/App.js
@@ -1,31 +1,29 @@
-import { BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
 import Home from './components/pages/Home'
 import Contact from './components/pages/Contact'
 import Company from './components/pages/Company'
 import NewProject from './components/pages/NewProject'
+import Projects from './components/pages/Projects'
 
 import Container from './components/layout/Container'
+import Navbar from './components/layout/Navbar'
+import Footer from './components/layout/Footer'
 
 function App() {
   return (
     <Router> 
-      <div>
-        <Link to="/">Home</Link>
-        <Link to="/Company">Empresa</Link>
-        <Link to="/Contact">Contato</Link>
-        <Link to="/NewProject">Novo Projeto</Link>
-      </div>
-
+      <Navbar />
       <Container customClass='min-height'>
         <Routes>
             <Route path= "/" element={<Home/>}/> 
             <Route path= "/Company" element={<Company/>}/> 
             <Route path= "/Contact" element={<Contact/>}/> 
-            <Route path= "/NewProject" element={<NewProject/>}/>    
+            <Route path= "/NewProject" element={<NewProject/>}/>  
+            <Route path= "/Projects" element={<Projects />}/>   
         </Routes>
       </Container>
-      <p>Footer</p>
+      <Footer />
 
     </Router>
   );
